565468ec8d79c323fb8f2c18d104b4dfb1752e23
Vatsal Adhiya <vatsaladhiya248@gmail.com>
2024-06-27 15:02:51 +0530

    Update database.js

--- data/database.js
+++ data/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     database: 'blog',
-    user: 'root',
-    password: 'Vatsal@03082002'
+    user: 'ENTER YOUR USERNAME HERE',
+    password: 'ENTER YOU PASSWOR HERE'
 });
 
 module.exports = pool;
